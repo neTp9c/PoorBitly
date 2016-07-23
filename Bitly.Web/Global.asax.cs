83e6083dc11e35b8b6a44bd7722ddcc3b0e44151
Alex Petryakov <alexpetryakov@gmail.com>
2016-07-23 23:50:18 +0300

    Add minimal stuff for displaying the homepage.

---
+++ Bitly.Web/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using Bitly.Web.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Bitly.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
